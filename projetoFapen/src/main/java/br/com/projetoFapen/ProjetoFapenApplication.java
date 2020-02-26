b0ead53407d972d4083019478c03164db8ac0796
Atanes <aatanes100@gmail.com>
2020-02-26 15:17:47 -0300

    Inicio do projeto com a inclusão das classes iniciais

---
+++ projetoFapen/src/main/java/br/com/projetoFapen/ProjetoFapenApplication.java
@@ -0,0 +1,13 @@
+package br.com.projetoFapen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoFapenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoFapenApplication.class, args);
+	}
+
+}
---
+++ projetoFapen/src/test/java/br/com/projetoFapen/ProjetoFapenApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.projetoFapen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoFapenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
