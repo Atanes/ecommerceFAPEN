8019454b9509bf043bdcf6fea5c48e0e324f2ae5
Atanes <aatanes100@gmail.com>
2020-02-27 14:15:02 -0300

    Inclusão da classe Produto, seus controles e pagina de cadastro. Inclusão das classes de controle para uso dos WEBJARS (Bootstrap e FontAwsome) Inclusão das informações do banco de dados no application.properties

---
+++ projetoFapen/src/main/java/br/com/projetoFapen/modelos/Produto.java
@@ -0,0 +1,77 @@
+package br.com.projetoFapen.modelos;
+
+import java.math.BigDecimal;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Produto {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	long id;
+	
+	String descricao;
+	
+	BigDecimal valor;
+	
+	float qtd;
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+	public BigDecimal getValor() {
+		return valor;
+	}
+
+	public void setValor(BigDecimal valor) {
+		this.valor = valor;
+	}
+
+	public float getQtd() {
+		return qtd;
+	}
+
+	public void setQtd(float qtd) {
+		this.qtd = qtd;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + (int) (id ^ (id >>> 32));
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Produto other = (Produto) obj;
+		if (id != other.id)
+			return false;
+		return true;
+	}
+	
+}
