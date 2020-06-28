c8bb96711558399630c01aaeaf1f3f713ebabba6
aatan <aatan@LAPTOP-I2Q04JVD>
2020-06-28 17:22:13 -0300

    Inclusão de um método no GrupoRepositório

--- projetoFapen/src/main/java/br/com/projetoFapen/repositorios/GrupoRepositorio.java
+++ projetoFapen/src/main/java/br/com/projetoFapen/repositorios/GrupoRepositorio.java
@@ -1,9 +1,13 @@
 package br.com.projetoFapen.repositorios;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import br.com.projetoFapen.modelos.Grupo;
 
 public interface GrupoRepositorio extends JpaRepository<Grupo, Long> {
+	
+	List<Grupo> findByDescricao(String Descricao);
 	
 }
